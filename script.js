a57d502b52591664c3056be4792f900a7b43283e
Godfred Amankwaa <godfred.amankwaa73@gmail.com>
2024-03-03 20:21:25 +0100

    changed autoplay settings

--- script.js
+++ script.js
@@ -10,14 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Call the function to set video source on DOMContentLoaded
     setVideoSource();
 
-    // Play the video automatically (and ensure it's muted for iOS)
-    video.play().then(function() {
-        // Autoplay started successfully
-    }).catch(function(error) {
-        // Autoplay was prevented
-        console.log("Autoplay was prevented: ", error);
-    });
-
-    // Mute the video for iOS devices to comply with autoplay policy
-    video.muted = true;
+    // Play the video automatically
+    video.play();
 });
